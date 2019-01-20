4f01f183f60530555bf9a11406aa8c91cff1a291
rhaegar453 <shivarajapple@gmail.com>
2019-01-20 14:07:57 +0530

    Back button bug fixed. Forward button needs to be taken care of

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -1,46 +1,66 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {paginateDecrement, paginateIncrement } from '../Store/Actions/uiActions';
-import {fetchAll} from '../Store/Actions/apiFetch';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  paginateDecrement,
+  paginateIncrement
+} from "../Store/Actions/uiActions";
+import { fetchAll } from "../Store/Actions/apiFetch";
 
-class Pagination extends React.Component{
-    constructor(props){
-        super(props);
-    }
+class Pagination extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-    PageDecrement=()=>{
-        this.props.paginateDecrement();
-        this.props.fetchAll(this.props.pageValue);
-    }
-    PageIncrement=()=>{
-        this.props.paginateIncrement();
-        this.props.fetchAll(this.props.pageValue);
-    }
-    render(){
-        return(
-            <div>
-                <div className="d-flex justify-content-center" style={{marginTop:"30px"}}>
-                <span onClick={this.PageDecrement}><i className="fas fa-caret-square-left fa-3x" style={{marginLeft:"30px"}}></i></span>
-                    <span onClick={this.PageIncrement}><i className="fas fa-caret-square-right fa-3x" style={{marginLeft:"30px"}}></i></span>
-                </div>
-            </div>
-        );
-    }
+  PageDecrement = () => {
+    this.props.paginateDecrement();
+    this.props.fetchAll(this.props.pageValue);
+  };
+  PageIncrement = () => {
+    this.props.paginateIncrement();
+    this.props.fetchAll(this.props.pageValue);
+  };
+  render() {
+      const backwardComponet=this.props.pageValue==0?null:          <span onClick={this.PageDecrement}>
+      <i
+        className="fas fa-caret-square-left fa-3x"
+        style={{ marginLeft: "30px" }}
+      />
+    </span>;
+    return (
+      <div>
+        <div
+          className="d-flex justify-content-center"
+          style={{ marginTop: "30px" }}
+        >
+        {backwardComponet}
+          <span onClick={this.PageIncrement}>
+            <i
+              className="fas fa-caret-square-right fa-3x"
+              style={{ marginLeft: "30px" }}
+            />
+          </span>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps=(state)=>{
-    return{
-        restaurants:state.api.restaurants,
-        pageValue:state.ui.pageValue
-    }
-}
+const mapStateToProps = state => {
+  return {
+    restaurants: state.api.restaurants,
+    pageValue: state.ui.pageValue
+  };
+};
 
-const mapDispatchToProps=(dispatch)=>{
-    return{
-        paginateIncrement:()=>dispatch(paginateIncrement()),
-        paginateDecrement:()=>dispatch(paginateDecrement()),
-        fetchAll:(id)=>dispatch(fetchAll(id))
-    }
-}
+const mapDispatchToProps = dispatch => {
+  return {
+    paginateIncrement: () => dispatch(paginateIncrement()),
+    paginateDecrement: () => dispatch(paginateDecrement()),
+    fetchAll: id => dispatch(fetchAll(id))
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Pagination);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Pagination);
